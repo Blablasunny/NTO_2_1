60ed19026ac3db3f16a8df1903767e4df00394a4
Blablasunny <91792426+Blablasunny@users.noreply.github.com>
2022-11-17 22:45:20 +0300

    версия 0.2

--- src/main/java/com/abp/App.java
+++ src/main/java/com/abp/App.java
@@ -1,7 +1,5 @@
 package com.abp;
 
-import com.abp.service.DemoService;
-import com.abp.service.LocationService;
 import org.h2.tools.Console;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
